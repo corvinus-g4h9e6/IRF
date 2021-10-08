986c247ff1115c3f6c8776c215f3bc484cf7a3b2
Bence Kiss <bence.kiss4@stud.uni-corvinus.hu>
2021-10-08 13:31:28 +0200

    6-excelthings

--- UserMaintenance/4hetexcel/Form1.cs
+++ UserMaintenance/4hetexcel/Form1.cs
@@ -20,14 +20,55 @@ namespace _4hetexcel
         RealEstateEntities context = new RealEstateEntities();
         List<Flat> Flats;
 
-        public Form1()
+        Excel.Application xlApp;
+        Excel.Workbook xlWB;
+        Excel.Worksheet xlSheet;
+
+
+
+        public Form1()  
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
         private void LoadData()
         {
             Flats = context.Flats.ToList();
+
+        }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                
+                xlApp = new Excel.Application();
+
+                
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+
+               
+                xlSheet = xlWB.ActiveSheet;
+
+                
+                //CreateTable();
+
+                
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+
+                
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+            }
 
         }
     }
