31c2106b0b5bf009aa338af1246adec8e1389bd9
Bence Kiss <bence.kiss4@stud.uni-corvinus.hu>
2021-10-29 22:27:54 +0200

    task7 done

--- UserMaintenance/week6MNB/Form1.cs
+++ UserMaintenance/week6MNB/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 using week6MNB.Entities;
 using week6MNB.MnbServiceReference;
@@ -21,14 +22,39 @@ namespace week6MNB
         public Form1()
         {
             InitializeComponent();
+            RefreshData();
+
+            
+        }
+        void RefreshData()
+        {
+            Rates.Clear();
 
             WebSeviceCall();
 
-            
+            Datagram();
 
-            dataGridView1.DataSource = Rates.ToList();
+            dataGridView1.DataSource = Rates;
         }
 
+        private void Datagram()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
 
         private void WebSeviceCall()
         {
@@ -36,9 +62,9 @@ namespace week6MNB
 
             var request = new GetExchangeRatesRequestBody()
             {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
+                currencyNames = comboBox1.SelectedItem.ToString(),
+                startDate = dateTimePicker1.Value.ToString(),
+                endDate = dateTimePicker2.Value.ToString()
             };
 
             var response = mnbService.GetExchangeRates(request);
@@ -70,6 +96,24 @@ namespace week6MNB
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+
         }
     }
 }
