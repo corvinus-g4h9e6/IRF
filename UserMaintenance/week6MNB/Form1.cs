67729a6c5638a3741603c1ee439cf6b28d8a7cf1
Bence Kiss <bence.kiss4@stud.uni-corvinus.hu>
2021-10-29 21:59:25 +0200

    task4 done

--- UserMaintenance/week6MNB/Form1.cs
+++ UserMaintenance/week6MNB/Form1.cs
@@ -13,11 +13,16 @@ namespace week6MNB
 {
     public partial class Form1 : Form
     {
+
+
+        BindingList<Entities.RateData> Rates = new BindingList<Entities.RateData>();
         public Form1()
         {
             InitializeComponent();
 
             WebSeviceCall();
+
+            dataGridView1.DataSource = Rates.ToList();
         }
 
         private void WebSeviceCall()
