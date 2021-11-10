44fdf402dc2be3b127fd57e80940f971d2f411e2
Bence Kiss <bence.kiss4@stud.uni-corvinus.hu>
2021-11-10 12:07:39 +0100

    car created

---
+++ UserMaintenance/week8Factory/Entities/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using week8Factory.Abstractions;
+
+namespace week8Factory.Entities
+{
+    class Car : Toy
+    {
+        protected override void DrawImage(Graphics g)
+        {
+            Image imageFile = Image.FromFile("Images/car.png");
+            g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
+        }
+    }
+}
